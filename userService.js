e1ca6e0340fee095dec7face63847d03f82d0f9a
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2014-08-13 14:25:48 +0200

    ensure user is logged in and add new API

--- userService.js
+++ userService.js
@@ -1,10 +1,24 @@
-function userService(db) {
+function userService(db, auth) {
 
     this.createNewUser = function () {
-        var user = new User({ name: 'new user' });
-        db.insert(user);
-        return user;
+
+        if (auth.isLoggedIn()) {
+            var user = new User({ name: 'new user' });
+            db.insert(user);
+            return user;
+        }
+
+        throw new Error("Login required");
     };
 
+    this.updateUser = function (user) {
+
+        if (auth.isLoggedIn()) {
+            db.update(user);
+            return user;
+        }
+
+        throw new Error("Login required");
+    };
 
 }
