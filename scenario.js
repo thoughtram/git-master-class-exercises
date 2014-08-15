dfe82d6d84e1767d7c92c0aed8156e0cb57eedb2
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2014-08-15 14:58:39 +0200

    add new API

--- scenario.js
+++ scenario.js
@@ -2,4 +2,8 @@ function catalogService (db) {
     this.getProduct = function(productId) {
         return db.find(productId);
     };
+
+    this.getProducts = function(productIds) {
+        return db.findBatch(productIds);
+    };
 }
