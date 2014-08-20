3e2e836d5bb24ce2c15021b269ceee0b63a92d45
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2014-08-21 01:01:33 +0200

    add addUser method

--- userService.js
+++ userService.js
@@ -1,4 +1,9 @@
 function userService (db) {
+
+    this.addUser = function (user) {
+        // not implemented
+    };
+
     this.getUser = function (id) {
         return db.users.get(id);
     };
